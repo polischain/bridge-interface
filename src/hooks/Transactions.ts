be14689e6b1111aed4965523696e427bdb2a539d
Aether-eum <aether@polispay.org>
2021-09-25 20:36:48 -0500

    Unused Imports

--- src/hooks/Transactions.ts
+++ src/hooks/Transactions.ts
@@ -1,19 +1,5 @@
-import { ChainId, Currency, CurrencyAmount, JSBI, Pair, Token, Trade } from 'hadeswap-beta-sdk'
-import { useMemo } from 'react'
-import { useUserSingleHopOnly } from 'state/user/hooks'
-import { isTradeBetter } from 'utils/trades'
-import { PairState, usePairs } from '../data/Reserves'
-import { wrappedCurrency } from '../utils/wrappedCurrency'
-import useSWR from 'swr'
-import  { abi as BridgeForeignABI }  from '../constants/abis/bridge_foreign.json'
-import  { abi as BridgeHomeABI }  from '../constants/abis/bridge_home.json'
-import { useActiveWeb3React } from './useActiveWeb3React'
-import { useUnsupportedTokens } from './Tokens'
-import { BigNumber, ethers } from 'ethers'
+import { CurrencyAmount, JSBI, Pair, Token, Trade } from 'hadeswap-beta-sdk'
 import useBridgeParams from '../hooks/useBridgeParams'
-import Fraction from '../entities/Fraction'
-import { BRIDGE_ADDRESS, BRIDGE_SENT_QUERY, CHAIN_BRIDGES } from '../constants'
-
 
 export function useIsTransactionUnsupported(amountIn?: CurrencyAmount): { daily: boolean; min: boolean; max: boolean } {
     const params = useBridgeParams()
